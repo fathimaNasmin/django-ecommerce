554a6a178d5e389d4b8e17bc97967f12cd21c189
Fathima nasmin <fathimanesmi@gmail.com>
2024-02-02 18:51:12 -0500

    custom permission for the admin user

---
+++ store/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework.permissions import BasePermission
+from rest_framework.permissions import SAFE_METHODS
+
+
+class AdminOnlyPermission(BasePermission):
+    """Custom permission for admin users."""
+    admin_methods = ('POST', 'PUT', 'PATCH', 'DELETE')
+    
+    def has_permission(self, request, view):
+        if (request.user.is_staff and 
+            request.user.is_superuser and
+            request.method in self.admin_methods):
+            return True 
+        elif request.method in SAFE_METHODS:
+            return True
+        
+    def has_object_permission(self, request, view, obj):
+        if (request.user.is_staff and 
+            request.user.is_superuser and
+            request.method in self.admin_methods):
+            return True 
+        
