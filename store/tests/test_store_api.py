3988ec6963e3987f97bd6bcf4f6d66bb29c9dd60
Fathima nasmin <fathimanesmi@gmail.com>
2024-02-02 18:52:10 -0500

    write test for posting subcategory

--- store/tests/test_store_api.py
+++ store/tests/test_store_api.py
@@ -12,6 +12,7 @@ from django.utils import timezone
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from django.contrib.auth import get_user_model
 from store.models import (
     Category, 
     SubCategory, 
@@ -30,6 +31,11 @@ from store.serializers import (
 CATEGORY_URL = reverse('store:category-list')
 SUB_CATEGORY_URL = reverse('store:sub-category-list')
 PRODUCT_URL = reverse('store:product-list')
+
+
+def category_detail_url(id):
+    """create and return category detail url."""
+    return reverse('store:category-detail', args=[id])
 
 
 def create_test_image():
@@ -93,6 +99,14 @@ class PublicAPITests(TestCase):
     """Test the endpoints for unauthenticated users."""
     def setUp(self):
         self.client = APIClient()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='admin123455'
+        )
+        self.normal_user = get_user_model().objects.create_user(
+            email='normaluser@example.com',
+            password='user@1234'
+        )
         
     def test_get_category(self):
         """test listing category api."""
@@ -106,6 +120,63 @@ class PublicAPITests(TestCase):
         
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+        
+    def test_create_category_admin_user(self):
+        """Test create category only by admin user."""
+        self.client.force_authenticate(self.admin_user)
+        
+        payload = {
+            'name': 'toys'
+        }
+        res = self.client.post(CATEGORY_URL, payload)
+        
+        category = Category.objects.get(id=res.data['id'])
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(res.data['name'], category.name)
+        
+    def test_patch_category_admin_user(self):
+        """Test editing the category name by the admin user."""
+        clothing = create_category(name='clothing')
+        
+        self.client.force_authenticate(self.admin_user)
+        
+        payload = {'name': 'fashion'}
+        url = category_detail_url(clothing.id)
+        res = self.client.patch(url, payload)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        clothing.refresh_from_db()
+        self.assertEqual(clothing.name, payload['name'])
+    
+    def test_delete_category_admin_user(self):
+        """Test deleting the category by the admin user."""
+        clothing = create_category(name='clothing')
+        
+        self.client.force_authenticate(self.admin_user)
+        
+        url = category_detail_url(clothing.id)
+        res = self.client.delete(url)
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        clothing_exists = Category.objects.filter(name='clothing').exists()
+        self.assertFalse(clothing_exists)
+
+    def test_create_category_no_user_raises_error(self):
+        """Test create category by anonymous user."""
+        payload = {
+            'name': 'games'
+        }
+        res = self.client.post(CATEGORY_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+        
+    def test_create_category_normal_user_error(self):
+        """Test create category raises error for normal users."""
+        self.client.force_authenticate(self.normal_user)
+        
+        payload = {
+            'name': 'toys'
+        }
+        res = self.client.post(CATEGORY_URL, payload)
+        
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
         
     def test_get_sub_category(self):
         """Test listing subcategory."""
@@ -121,6 +192,36 @@ class PublicAPITests(TestCase):
         
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+        
+    def test_post_sub_category(self):
+        """Test posting sub_category."""
+        self.client.force_authenticate(self.admin_user)
+        fashion = create_category(name='abc12345')
+        
+        payload = {
+            'name': 'woomens',
+            'category': fashion.id
+        }
+        
+        res = self.client.post(SUB_CATEGORY_URL, payload, format='json')
+        
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        
+    def test_post_subcategory_normal_user_error(self):
+        """Test create category raises error for normal users."""
+        self.client.force_authenticate(self.normal_user)
+
+        fashion = create_category(name='xyzgjsd')
+        payload = {
+            'name': 'kdkfhsdh',
+            'category': {
+                'id': fashion.id,
+                'name': fashion.name
+            }
+        }
+        res = self.client.post(SUB_CATEGORY_URL, payload, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
         
     def test_get_products(self):
         """Test listing all products."""
@@ -152,4 +253,6 @@ class PublicAPITests(TestCase):
                          serializer.data[0]['discount'])
         self.assertEqual(res.data[0]['discount'][0]['percent'], 
                          str(discount.percent))
+        
+    
         
