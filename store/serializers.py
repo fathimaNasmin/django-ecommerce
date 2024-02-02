a13dc4023ed0405664201186eb9ccc6fb6c84e73
Fathima nasmin <fathimanesmi@gmail.com>
2024-02-02 18:52:27 -0500

    serializer for post subcategory

--- store/serializers.py
+++ store/serializers.py
@@ -1,54 +1,56 @@
 """Serializers for Store Models."""
+from django.forms.models import model_to_dict
 from django.utils import timezone
 from store.models import Category, SubCategory, Product, Inventory, Discount
 
 from rest_framework import serializers
 
-     
+
 class CategorySerializer(serializers.ModelSerializer):
     """Serializer for Category."""
     class Meta:
         model = Category
         fields = ['id', 'name']
-     
-        
+
+
 class SubCategorySerializer(serializers.ModelSerializer):
     """Serializer for subcategory."""
-    category = CategorySerializer()
-    
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+
     class Meta:
         model = SubCategory
         fields = ['id', 'name', 'category']
-        
-        
+
+
 class InventorySerializer(serializers.ModelSerializer):
     """Serializer for inventory."""
     created_at = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')
     modified_at = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')
-    
+
     class Meta:
         model = Inventory
         fields = ['id', 'quantity', 'created_at', 'modified_at']
-        
-        
+
+
 class DiscountSerializer(serializers.ModelSerializer):
     """Serializer for Discount."""
     created_at = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')
     modified_at = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')
     valid_till = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')
-    
+
     class Meta:
         model = Discount
-        fields = ['id', 'percent', 'active', 'created_at', 'modified_at', 'valid_till']
-        
+        fields = ['id', 'percent', 'active',
+                  'created_at', 'modified_at', 'valid_till']
+
     def validate_valid_till(self, date):
         """Validate valid_till date is a future date."""
         current_date = timezone.now().date()
-        
+
         if date <= current_date:
             raise serializers.ValidationError(
                 "The date should be a future date.")
-        
+
         return date
 
 
@@ -57,7 +59,7 @@ class ProductSerializer(serializers.ModelSerializer):
     sub_category = SubCategorySerializer()
     product_inventory = InventorySerializer(read_only=True, many=True)
     discount = DiscountSerializer(read_only=True, many=True)
-    
+
     class Meta:
         model = Product
         fields = ['id', 'name', 'price', 'image', 'description',
--- store/views.py
+++ store/views.py
@@ -5,33 +5,30 @@ from store.serializers import (CategorySerializer,
                                SubCategorySerializer,
                                ProductSerializer)
 
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, viewsets, status
+from rest_framework.response import Response
 
 from django.db.models import Prefetch
 
-# /category/ :GET
-# /category/<cateory_id>/ : view detail
+from store.permissions import AdminOnlyPermission
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     """View to lists all Category."""
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    
+    permission_classes = [AdminOnlyPermission]
+
 
 class SubCategoryViewSet(viewsets.ModelViewSet):
     """View to lists all subcategory."""
     queryset = SubCategory.objects.all().order_by('id')
     serializer_class = SubCategorySerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [AdminOnlyPermission]
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     """View to lists all products."""
     queryset = Product.objects.all().order_by('id')[:5]
-    # queryset = Product.objects.prefetch_related('inventory_set').order_by('id')[:5]
-    # queryset = Product.objects.select_related('inventory').order_by('id')[:5]
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    
-    
